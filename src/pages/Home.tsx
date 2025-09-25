181abccaf55e74d4fb9bdb75bfa98f77cf5675dc
Wayne Hsu <waynehsu@micb2b.com>
2025-09-25 11:50:29 +0800

    [feat] 首頁-新增排序功能, 套用電影排序選擇器

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,13 @@ import { useState, useEffect, useCallback } from 'react'
 import * as api from '../api/tmdb'
 import type { ConvertedMovieDetail } from '../types/models'
 import { MovieFilterType } from '../types/variables'
-import { Radio, Row, Col, Spin, FloatButton, Input } from 'antd'
+import { Radio, Row, Col, Spin, FloatButton, Input, Tooltip } from 'antd'
+import { InfoCircleOutlined } from '@ant-design/icons'
 import { useMessageContext } from '../contexts/MessageContext/MessageContext'
 import MovieDetailModal from './modal/MovieDetailModal'
 import MovieCard from '../components/MovieCard'
+import SortSelect from '../components/SortSelect'
+import type { SortOption } from '../components/SortSelect'
 
 export default function Home() {
   const { Search } = Input
@@ -21,6 +24,8 @@ export default function Home() {
   const [hasMore, setHasMore] = useState(true)
   const [selectedMovieId, setSelectedMovieId] = useState<number | null>(null)
   const [modalVisible, setModalVisible] = useState(false)
+  const [sortBy, setSortBy] = useState<SortOption>('release_desc')
+  const [isStale, setIsStale] = useState(false)
 
   useEffect(() => {
     setMovieList([])
@@ -64,6 +69,7 @@ export default function Home() {
         })
         setPage(pageNumber + 1)
         if (pageNumber >= res.total_pages) setHasMore(false)
+        setIsStale(true)
       } catch (err) {
         console.error(err)
         error('讀取資料失敗, 請稍後再試')
@@ -90,6 +96,39 @@ export default function Home() {
     return () => contentEl.removeEventListener('scroll', handleScroll)
   }, [loadMovies, page, targetEl])
 
+  const applySort = (list: ConvertedMovieDetail[], sortOption: SortOption) => {
+    switch (sortOption) {
+      case 'release_desc':
+        return list.sort(
+          (a, b) =>
+            new Date(b.release_date).getTime() -
+            new Date(a.release_date).getTime()
+        )
+      case 'release_asc':
+        return list.sort(
+          (a, b) =>
+            new Date(a.release_date).getTime() -
+            new Date(b.release_date).getTime()
+        )
+      case 'rating_desc':
+        return list.sort((a, b) => b.vote_average - a.vote_average)
+      case 'rating_asc':
+        return list.sort((a, b) => a.vote_average - b.vote_average)
+      case 'title_asc':
+        return list.sort((a, b) => a.title.localeCompare(b.title, 'zh'))
+      case 'title_desc':
+        return list.sort((a, b) => b.title.localeCompare(a.title, 'zh'))
+      default:
+        return list
+    }
+  }
+
+  const handleSortChange = (option: SortOption) => {
+    setSortBy(option)
+    setMovieList((prev) => applySort(prev, option))
+    setIsStale(false)
+  }
+
   const openModal = (movieId: number) => {
     setSelectedMovieId(movieId)
     setModalVisible(true)
@@ -106,7 +145,7 @@ export default function Home() {
         <Col xs={24}>
           <h1 className='title'>電影列表</h1>
         </Col>
-        <Col xs={24} md={16}>
+        <Col xs={24} md={12}>
           <Radio.Group
             value={movieFilterType}
             onChange={(e) => setMovieFilterType(e.target.value)}
@@ -124,8 +163,8 @@ export default function Home() {
             <Radio.Button value={MovieFilterType.SEARCH}>搜尋電影</Radio.Button>
           </Radio.Group>
         </Col>
-        {movieFilterType === MovieFilterType.SEARCH && (
-          <Col xs={24} md={8}>
+        <Col xs={24} md={6}>
+          {movieFilterType === MovieFilterType.SEARCH && (
             <Search
               placeholder='請輸入關鍵字'
               enterButton='搜尋'
@@ -133,8 +172,31 @@ export default function Home() {
               className='search'
               allowClear
             />
-          </Col>
-        )}
+          )}
+        </Col>
+        <Col xs={24} md={6} className='select-group'>
+          <SortSelect
+            value={sortBy}
+            onChange={handleSortChange}
+            className='select'
+          />
+          <Tooltip
+            title={
+              movieList.length > 0 && isStale
+                ? '列表已變更，請點擊重新套用排序'
+                : '排序僅依照目前已載入的列表'
+            }
+          >
+            <InfoCircleOutlined
+              className={`tooltip-icon ${
+                movieList.length > 0 && isStale ? 'isStale' : ''
+              }`}
+              onClick={() => {
+                if (isStale) handleSortChange(sortBy)
+              }}
+            />
+          </Tooltip>
+        </Col>
       </Row>
       <Row gutter={[16, 16]}>
         {movieList.map((movie) => (
